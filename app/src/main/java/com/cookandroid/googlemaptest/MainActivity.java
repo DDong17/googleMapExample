d7be59d51e6fb34f7b5ada5599c3fea1b2b1010a
mopil1102 <mopil1102@gmail.com>
2022-03-27 17:00:09 +0900

    Initial commit

---
+++ app/src/main/java/com/cookandroid/googlemaptest/MainActivity.java
@@ -0,0 +1,53 @@
+package com.cookandroid.googlemaptest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+    Button showMapButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        showMapButton = (Button) findViewById(R.id.btnShowMap);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng SEOUL = new LatLng(37.56, 126.97);
+
+        // 마커 설정
+        MarkerOptions markerOptions = new MarkerOptions(); // 마커 생성
+        markerOptions.position(SEOUL);
+        markerOptions.title("서울"); // 마커 제목
+        markerOptions.snippet("한국의 수도"); // 마커 설명
+        mMap.addMarker(markerOptions);
+
+        // 초기 위치
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
+
+        // 줌의 정도
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+        // 지도 표시 형식 변경
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+    }
+}
